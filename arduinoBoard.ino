f6561ca340d35a6edf7cc6321ea930350c505f87
shieldce <tiantiandayou@qq.com>
2019-09-07 21:22:21 +0800

    增加上传和命令

--- arduinoBoard.ino
+++ arduinoBoard.ino
@@ -1,19 +1,25 @@
 #include <ArduinoBearSSL.h>
 #include <ArduinoECCX08.h>
 #include <ArduinoMqttClient.h>
+#include <ArduinoJson.h>
 #include <MKRNB.h>
+#include "WDTZero.h"
 
 // NB settings
 #define SECRET_PINNUMBER ""
 
 // Fill in the hostname of your broker
-#define SECRET_BROKER "106.12.34.4"
-#define MQTT_USER "luhuafeng"
-#define MQTT_PASS "125436qq!"
-#define SUBSCRIBE_TOPIC_COMMAND "mtopic/cmdtopic"
-#define PUBLISH_TOPIC_REPORT "mtopic/rpttopic"
+#define SECRET_BROKER "47.88.225.240"
+#define SECRET_PORT 1883
+#define MQTT_USER "28d39cd5-3757-42de-9a59-1bb1fdefd4db"
+#define MQTT_PASS "486d392b-23fb-4734-815c-f00f4f793f46"
+#define SUBSCRIBE_TOPIC_COMMAND "channels/776396e2-979d-4991-94c3-7318ea1746aa/messages"
+#define PUBLISH_TOPIC_REPORT "channels/ba22f57d-642e-4b82-9718-5e3b68809ac0/messages"
 
 #define SSL_CONNECT_NONE
+
+#define LED_ON 1
+#define LED_OFF 0
 
 // Fill in the boards public certificate
 const char SECRET_CERTIFICATE[] = R"(
@@ -47,9 +53,13 @@ MqttClient    mqttClient(nbClient);
 
 unsigned long lastMillis = 0;
 
+int pump1 = 0;
+int pump2 = 0;
+int pump3 = 0;
+
+WDTZero MyWatchDoggy; // Define WDT
+
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);
   if (!ECCX08.begin()) {
     while (1);
   }
@@ -70,15 +80,19 @@ void setup() {
   // called when the MQTTClient receives a message
   mqttClient.onMessage(onMessageReceived);
   // DI Configure
-  for (int idx = 0; idx <= 1; idx++) {
-    pinMode(idx, OUTPUT);
-  }
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  MyWatchDoggy.setup(WDT_SOFTCYCLE1M);  // initialize WDT-softcounter refesh cycle on 1m interval
+  ledTwinkle(1, 5000);
 }
 
 void loop() {
   if (nbAccess.status() != NB_READY || gprs.status() != GPRS_READY) connectNB();
   if (!mqttClient.connected()) connectMQTT();
   mqttClient.poll(); // poll for new MQTT messages and send keep alives
+  MyWatchDoggy.clear();  // refresh wdt - before it loops
   if (millis() - lastMillis > 5000) { // publish a message roughly every 5 seconds.
     lastMillis = millis();
     publishMessage();
@@ -92,47 +106,66 @@ unsigned long getTime() {
 void connectNB() {
   while ((nbAccess.begin(pinnumber) != NB_READY) ||
          (gprs.attachGPRS() != GPRS_READY)) {
-    delay(1000);
+    ledTwinkle(3, 500);
   }
 }
 
 void connectMQTT() {
-  while (!mqttClient.connect(broker, 1883)) delay(5000);
+  while (!mqttClient.connect(broker, SECRET_PORT)) {
+    ledTwinkle(6, 300);
+  }
   mqttClient.subscribe(SUBSCRIBE_TOPIC_COMMAND);
 }
 
 void publishMessage() {
-  Serial.println("Read AO");
-  int val1 = analogRead(A0);
-  int val2 = analogRead(A1);
-  Serial.println(val1);
-  Serial.println(val2);
-  Serial.println("Send Topic");
   mqttClient.beginMessage(PUBLISH_TOPIC_REPORT);
-  mqttClient.print("{\"Level1\":\"");
-  mqttClient.print(val1 / 100);
-  mqttClient.print(".");
-  mqttClient.print(val1 % 100);
-  mqttClient.print("\",\"Level2\":\"");
-  mqttClient.print(val2 / 100);
-  mqttClient.print(".");
-  mqttClient.print(val2 % 100);
-  mqttClient.print("\"}");
+  const int capacity = JSON_ARRAY_SIZE(3) + 3*JSON_OBJECT_SIZE(3);
+  StaticJsonDocument<capacity> doc;
+  int val1 = analogRead(A0);
+  JsonObject waterLevel = doc.createNestedObject();
+  waterLevel["n"] = "water_level";
+  waterLevel["u"] = "cm";
+  waterLevel["v"] = val1;
+  int val2 = analogRead(A1);
+  JsonObject current = doc.createNestedObject();
+  current["n"] = "pump_current";
+  current["u"] = "A";
+  current["v"] = val2;
+  JsonObject pumpStatus = doc.createNestedObject();
+  pumpStatus["n"] = "pump_status";
+  pumpStatus["u"] = "";
+  pumpStatus["v"] = pump1 + pump2 * 10 + pump3 * 100;
+  doc.add(waterLevel);
+  doc.add(current);
+  doc.add(pumpStatus);
+  String forprint="";
+  serializeJson(doc, forprint);
+  mqttClient.print(forprint);
   mqttClient.endMessage();
+  ledTwinkle(3,200);
 }
 
 void onMessageReceived(int messageSize) {
-  Serial.println("Receive Topic");
-  byte commandContent[2];
-  int i = 0;
-  while ((i <= 2) && (mqttClient.available())){
-    commandContent[i] = mqttClient.read();
-    i++;
-  }
-  Serial.print(commandContent[0] - 48);
-  Serial.print(commandContent[1] - 48);
-  Serial.println();
-  if (((commandContent[0] >= 48) && (commandContent[0] <= 49)) && ((commandContent[1] == 48) || (commandContent[1] == 49))) {
-    digitalWrite((commandContent[0] - 48), (commandContent[1] - 48));
+  const int capacity = JSON_OBJECT_SIZE(4);
+  StaticJsonDocument<capacity> doc;
+  deserializeJson(doc, mqttClient);
+  pump1 = doc["p1"].as<int>();
+  pump2 = doc["p2"].as<int>();
+  pump3 = doc["p3"].as<int>();
+  digitalWrite(0, pump1);
+  digitalWrite(1, pump2);
+  digitalWrite(2, pump3);
+  ledTwinkle(5,200);
+  publishMessage();
+}
+
+void ledTwinkle(int onoffloopnum, int delaytime){
+  for (int i = 0; i < onoffloopnum; i++){
+    if (i != 0) {
+      delay(delaytime);
+    }
+    digitalWrite(LED_BUILTIN, LED_ON);
+    delay(delaytime);
+    digitalWrite(LED_BUILTIN, LED_OFF);
   }
 }
